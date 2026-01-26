4083f871c4776e35a765204d418f0bd9d38dd828
Chuong <chuongww23@gmail.com>
2026-01-26 15:22:34 +0700

    Change URL for deployment purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-
-import { PrimeReactProvider } from 'primereact/api';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login/Login';
-import Home from './pages/Home';
+import { PrimeReactProvider } from "primereact/api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home";
 // import SemesterManagement from './pages/SemesterManagement';
-import Teams from './pages/Teams';
-import TeamDetail from './pages/TeamDetail';
-import ProtectedRoute from './components/ProtectedRoute';
+import Teams from "./pages/Teams";
+import TeamDetail from "./pages/TeamDetail";
 
 function App() {
   return (
@@ -16,17 +14,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/teams" element={<Teams />} />
-            <Route path="/teams/:teamId" element={<TeamDetail />} />
-          </Route>
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/teams/:teamId" element={<TeamDetail />} />
           {/* <Route element={<ProtectedRoute allowedRoles={['HOD']} />}>
             <Route path="/semesters" element={<SemesterManagement />} />
           </Route> */}
         </Routes>
       </BrowserRouter>
     </PrimeReactProvider>
-  )
+  );
 }
 
-export default App
+export default App;
